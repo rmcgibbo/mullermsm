526fc585c45f5b5b9dd1623dd92b3b12ca9b8113
Robert McGibbon <rmcgibbo@gmail.com>
2012-09-02 16:51:55 -0700

    old setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from glob import glob
-from setuptools import setup
-#from distutils.core import setup
+from distutils.core import setup
 
 setup(name='mullermsm',
       version='0.1',
@@ -8,8 +7,24 @@ setup(name='mullermsm',
       include_package_data=True,
       package_data = {'': ['conf.pdb']},
       packages=['mullermsm'],
-      #install_requires=['theano', 'numpy', 'matplotlib', 'ipython'],
       package_dir={'mullermsm':'lib'},
-      scripts=['scripts/mullermsm_plot_assignments.py'],
-      #scripts=glob('scripts/*.py'))
-)
+      scripts=glob('scripts/*.py'))
+
+
+
+#from glob import glob
+#from setuptools import setup
+#from numpy.distutils.core import setup
+#from distutils.core import setup
+
+#setup(name='mullermsm',
+#      version='0.1',
+#      license='GPLv3',
+#      include_package_data=True,
+#      package_data = {'': ['conf.pdb']},
+#      packages=['mullermsm'],
+#      #install_requires=['theano', 'numpy', 'matplotlib', 'ipython'],
+#      package_dir={'mullermsm':'lib'},
+#      scripts=['scripts/mullermsm_plot_assignments.py'],
+#      #scripts=glob('scripts/*.py'))
+#)
